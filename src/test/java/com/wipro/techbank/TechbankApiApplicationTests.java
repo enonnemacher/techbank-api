2379a1ca69b43021c4f396ce336771f176ee33d4
enonnemacher <enonnemacher@gmail.com>
2022-04-17 08:14:59 -0300

    feat: create project

---
+++ src/test/java/com/wipro/techbank/TechbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.techbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
