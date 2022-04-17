d0c8ae574631f3c20e8f8740e6c109487bec5a51
renatomak <renato.mark.silva@gmail.com>
2022-04-17 16:49:05 -0300

    feat: add spring jpa annotations

--- src/main/java/com/wipro/techbank/domain/Client.java
+++ src/main/java/com/wipro/techbank/domain/Client.java
@@ -1,10 +1,35 @@
 package com.wipro.techbank.domain;
 
-public class Client {
+import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_client")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String cpf;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
+    @Column(nullable = false)
     private String email;
 }
