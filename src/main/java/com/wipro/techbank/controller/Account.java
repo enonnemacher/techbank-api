71c6c7954bf520ab4e71ca340cb5cb205bf57da2
enonnemacher <enonnemacher@gmail.com>
2022-04-17 09:33:28 -0300

    feat: Create class CheckingAccount

--- src/main/java/com/wipro/techbank/controller/Account.java
+++ src/main/java/com/wipro/techbank/controller/Account.java
@@ -1,6 +1,7 @@
 package com.wipro.techbank.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Account {
 
---
+++ src/main/java/com/wipro/techbank/controller/CheckingAccount.java
@@ -0,0 +1,13 @@
+package com.wipro.techbank.controller;
+
+public class CheckingAccount extends Account{
+    @Override
+    public void withDraw() {
+
+    }
+
+    @Override
+    public void transfer() {
+
+    }
+}
