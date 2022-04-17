f445c131b8bce9206068ffabbc91731c3fae7a31
enonnemacher <enonnemacher@gmail.com>
2022-04-17 09:21:46 -0300

    feat: Create class Client

---
+++ src/main/java/com/wipro/techbank/controller/Client.java
@@ -0,0 +1,10 @@
+package com.wipro.techbank.controller;
+
+public class Client {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private String phoneNumber;
+    private String email;
+}
