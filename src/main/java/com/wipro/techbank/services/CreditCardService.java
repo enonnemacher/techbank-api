c8bfc3a9816d5ca766254b9493eed61e21f90e68
renatomak <renato.mark.silva@gmail.com>
2022-04-18 20:24:13 -0300

    feat: credit card service delete

--- src/main/java/com/wipro/techbank/services/CreditCardService.java
+++ src/main/java/com/wipro/techbank/services/CreditCardService.java
@@ -4,8 +4,11 @@ import com.wipro.techbank.domain.CreditCard;
 import com.wipro.techbank.dtos.CreditCardResponseDto;
 import com.wipro.techbank.dtos.CreditCardRequestDto;
 import com.wipro.techbank.repositories.CreditCardRepository;
+import com.wipro.techbank.services.exceptions.DataBasesException;
 import com.wipro.techbank.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -42,8 +45,18 @@ public class CreditCardService {
     @Transactional(readOnly = true)
     public CreditCardResponseDto findById(Long id) {
         Optional<CreditCard> optionalCreditCard = creditCardRepository.findById(id);
-        CreditCard entity = optionalCreditCard.orElseThrow(() -> new ResourceNotFoundException("Entidade não econtrada"));
+        CreditCard entity = optionalCreditCard.orElseThrow(() -> new ResourceNotFoundException("Entidade não econtrada."));
         return new CreditCardResponseDto(entity);
+    }
+
+    public void delete(Long id) {
+        try {
+            creditCardRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id " + id + " não encontrado.");
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBasesException("Violação de integridade.");
+        }
     }
 
     
