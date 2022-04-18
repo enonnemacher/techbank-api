7bbf864149dfbcd5b8204fb67886fef27d530ad6
renatomak <renato.mark.silva@gmail.com>
2022-04-18 15:39:36 -0300

    feat: JPA and Lombok annotations included

--- src/main/java/com/wipro/techbank/domain/CreditCard.java
+++ src/main/java/com/wipro/techbank/domain/CreditCard.java
@@ -1,13 +1,43 @@
 package com.wipro.techbank.domain;
+import lombok.*;
 
-import java.util.Date;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
-public class CreditCard {
+@Entity
+@Table(name = "tb_credit_card")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class CreditCard implements Serializable {
+    private static final long serialVersionUID = 3L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(nullable = false)
     private String cardNumber;
-    private Date expirationDate;
+
+    @Column(nullable = false)
+    private LocalDateTime expirationDate;
+
+    @Column(nullable = false)
     private Short securityCode;
-    private Double limit;
-    private Double usedLimit = 0d;
+
+    @Column(nullable = false)
+    private Double limitCredit;
+
+    private Double usedLimit;
+
+    public void toBuy(Double value) {
+        this.usedLimit += value;
+    }
+
 }
+
