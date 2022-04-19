deeeea16d6c8afe26321327980ba51d079060c63
enonnemacher <enonnemacher@gmail.com>
2022-04-19 19:04:12 -0300

    refact: Class ClientService

--- src/main/java/com/wipro/techbank/services/ClientService.java
+++ src/main/java/com/wipro/techbank/services/ClientService.java
@@ -2,6 +2,7 @@ package com.wipro.techbank.services;
 
 import com.wipro.techbank.domain.Client;
 import com.wipro.techbank.repositories.ClientRepository;
+import com.wipro.techbank.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,32 +14,26 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    public Client save(Client client){
+    public Client save(Client client) {
         return clientRepository.save(client);
     }
 
-    public List<Client> findAll(){
+    public List<Client> findAll() {
         List<Client> clientsList = clientRepository.findAll();
         return clientsList;
     }
 
-    public Client findById(Long id){
-        //return clientRepository.findById(id).orElseThrow() -> NotFoundException("client");
+    public Client findById(Long id) {
+        return clientRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Client not found!"));
     }
 
-    public Client update(Long id, Client client){
+    public Client update(Long id, Client client) {
         Client findClient = findById(id);
-        //efetuar validação da busca
-        return  clientRepository.save(client);
+        return clientRepository.save(client);
     }
 
-    public void remove(Long id){
+    public void remove(Long id) {
         Client client = findById(id);
-        // efetuar validação
-        /*
-        if (clientRepository.findById(id)){
-            throw new NotFoundException();
-        }*/
         clientRepository.deleteById(id);
     }
 }
