cc786f7c5b1ea47f046875b2f652bdd83efded30
renatomak <renato.mark.silva@gmail.com>
2022-04-21 17:35:59 -0300

    test: change name test

---
+++ src/test/java/com/wipro/techbank/repositories/CreditCardRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.wipro.techbank.repositories;
+
+import com.wipro.techbank.domain.CreditCard;
+import com.wipro.techbank.tests.Factory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreditCardRepositoryTest {
+//
+//    @Autowired
+//    private CreditCardRepository creditCardRepository;
+//
+//    private long existingId;
+//    private long nonexistingId;
+//    private long countTotalCreditCards;
+//
+//    @BeforeEach
+//    void setUp() {
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    @Test
+//    public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+//        CreditCard creditCard = Factory.createCreditCard();
+//        creditCard.setId(null);
+//        creditCard = creditCardRepository.save(creditCard);
+//
+//        Assertions.assertNotNull(creditCard.getId());
+//        Assertions.assertEquals(countTotalCreditCards + 1, creditCard.getId());
+//    }
+}
