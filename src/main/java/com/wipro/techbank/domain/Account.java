59efca5af2e700b421e1c3bd4db0969e648c2365
renatomak <renato.mark.silva@gmail.com>
2022-04-22 20:25:13 -0300

    feat: imposts

--- src/main/java/com/wipro/techbank/domain/Account.java
+++ src/main/java/com/wipro/techbank/domain/Account.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "tb_account")
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Account  implements Serializable {
     private static final long serialVersionUID = -6666350505838863149L;
