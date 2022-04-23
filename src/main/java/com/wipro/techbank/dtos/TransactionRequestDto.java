b1c205f11f5490eb0a1e0f7ada1f884cb3b4be09
renatomak <renato.mark.silva@gmail.com>
2022-04-23 14:26:40 -0300

    feat: transaction request dto

---
+++ src/main/java/com/wipro/techbank/dtos/TransactionRequestDto.java
@@ -0,0 +1,36 @@
+package com.wipro.techbank.dtos;
+
+import com.wipro.techbank.domain.AccountType;
+import com.wipro.techbank.domain.Operation;
+import com.wipro.techbank.domain.Transaction;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class TransactionRequestDto implements Serializable {
+    private static final long serialVersionUID = 377217321776086460L;
+
+    @NotNull(message = "Descrição da operação obrigatória.")
+    private Operation operation;
+
+    @NotNull(message = "Valor da operação é obrigatório.")
+    private Double value;
+
+    @NotNull(message = "O tipo da conta é obrigatório.")
+    private AccountType accountType;
+
+
+    public TransactionRequestDto(Transaction entity) {
+        operation = entity.getOperation();
+        value = entity.getValue();
+        accountType = entity.getAccountType();
+    }
+}
