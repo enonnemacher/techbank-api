a3456796f10745b476386c363a94fa7b9f7f013d
renatomak <renato.mark.silva@gmail.com>
2022-04-23 14:24:58 -0300

    feat: transaction method doposit

--- src/main/java/com/wipro/techbank/services/TransactionService.java
+++ src/main/java/com/wipro/techbank/services/TransactionService.java
@@ -1,8 +1,11 @@
 package com.wipro.techbank.services;
 
 
-import com.wipro.techbank.domain.Transaction;
-import com.wipro.techbank.dtos.TransactionDto;
+import com.wipro.techbank.domain.*;
+import com.wipro.techbank.dtos.TransactionRequestDto;
+import com.wipro.techbank.dtos.TransactionResponseDto;
+import com.wipro.techbank.repositories.CheckingAccountRepository;
+import com.wipro.techbank.repositories.SpecialAccountRepository;
 import com.wipro.techbank.repositories.TransactionRepository;
 import com.wipro.techbank.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,19 +22,48 @@ public class TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
 
-    public Page<TransactionDto> findAllPaged(Pageable pageable) {
+    @Autowired
+    private CheckingAccountRepository checkingAccountRepository;
+
+    @Autowired
+    private SpecialAccountRepository specialAccountRepository;
+
+    public Page<TransactionResponseDto> findAllPaged(Pageable pageable) {
         Page<Transaction> operations = transactionRepository.findAll(pageable);
-        return operations.map(TransactionDto::new);
+        return operations.map(TransactionResponseDto::new);
     }
 
     @Transactional(readOnly = true)
-    public TransactionDto findById(Long id) {
+    public TransactionResponseDto findById(Long id) {
         Optional<Transaction> transaction = transactionRepository.findById(id);
         Transaction entity = transaction.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-        return new TransactionDto(entity);
+        return new TransactionResponseDto(entity);
     }
 
-    private void copyDtoToEntity(TransactionDto dto, Transaction entity) {
+    public TransactionResponseDto deposit(Long accountId, TransactionRequestDto transactionDto) {
+        Account account = null;
+        Transaction transaction = new Transaction();
+
+        transaction.setOperation(Operation.DEPOSIT);
+        transaction.setValue(transactionDto.getValue());
+        transaction.setAccountType(transactionDto.getAccountType());
+
+        if (transactionDto.getAccountType().equals(AccountType.CHECKING_ACCOUNT)) {
+            account = checkingAccountRepository.getById(accountId);
+            account.deposit(transactionDto.getValue());
+            transaction.setAccount(account);
+            transaction = transactionRepository.save(transaction);
+        }
+        if (transactionDto.getAccountType().equals(AccountType.ESPECIAL_ACCOUNT)) {
+            account = specialAccountRepository.getById(accountId);
+            account.deposit(transactionDto.getValue());
+            transaction.setAccount(account);
+            transaction = transactionRepository.save(transaction);
+        }
+        return new TransactionResponseDto(transaction, account.getBalance());
+    }
+
+    private void copyDtoToEntity(TransactionResponseDto dto, Transaction entity) {
         entity.setOperation(dto.getOperation());
         entity.setValue(dto.getValue());
     }
