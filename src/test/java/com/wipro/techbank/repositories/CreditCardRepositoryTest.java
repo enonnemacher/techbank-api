17ad30813580b1da4606e195e394bfb4c5f482c1
renatomak <renato.mark.silva@gmail.com>
2022-04-24 19:44:59 -0300

    test: method delete when id exists

--- src/test/java/com/wipro/techbank/repositories/CreditCardRepositoryTest.java
+++ src/test/java/com/wipro/techbank/repositories/CreditCardRepositoryTest.java
@@ -4,13 +4,16 @@ import com.wipro.techbank.domain.CreditCard;
 import com.wipro.techbank.tests.Factory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@DataJpaTest
 class CreditCardRepositoryTest {
 //
 //    @Autowired
