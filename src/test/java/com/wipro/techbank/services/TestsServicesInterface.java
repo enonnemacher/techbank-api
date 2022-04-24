01bfe6eae9f8fe79a4aa537d7f7a39f0daf36a51
renatomak <renato.mark.silva@gmail.com>
2022-04-24 18:47:39 -0300

    feat: create test Service interface

---
+++ src/test/java/com/wipro/techbank/services/TestsServicesInterface.java
@@ -0,0 +1,23 @@
+package com.wipro.techbank.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public interface TestsServicesInterface {
+
+    void setUp();
+
+    void findAllShouldReturnPage();
+
+    void findByIdShouldReturnCreditCardResponseDtoWhenIdExixts();
+
+    void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExixts();
+
+    void createShouldReturnCreditCardResponseDto();
+
+    void deleteShouldThrowDataBasesExceptionWhenIdIsDependent();
+
+    void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists();
+
+    void deleteShouldDoNothingWhenIdExists();
+}
